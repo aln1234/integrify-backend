8c742356bfbd47ad43bebfc4d5a0d01945ebab44
Albin Lamichhane <albin.lamichhane@tuni.fi>
2023-11-24 20:36:59 +0200

    added changes

--- test/mockData/cartData.ts
+++
@@ -1,8 +0,0 @@
-export default [
-  {
-    product: "654cc82bda327008036fb97e",
-    user: "654cc82bda327008036fb97c",
-    quantity: 1,
-    total: 100,
-  },
-];
--- test/mockData/categoryData.ts
+++
@@ -1,5 +0,0 @@
-export default [
-  { name: "Clothes" },
-  { name: "Electronics" },
-  { name: "Accessories" },
-];
